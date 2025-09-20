79440b3c17959794432273540e50911172cf9494
G`ofur <2004gofur@gmail.com>
2025-09-20 15:48:59 +0500

    examplni o'chirdim

--- src/main/java/uz/pdp/Example.java
+++ src/main/java/uz/pdp/Example.java
@@ -7,13 +7,6 @@ import java.util.concurrent.ForkJoinPool;
 public class Example {
     public static void main(String[] args) throws InterruptedException {
 
-        CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(() -> {
-                    return new Random().nextInt(100);
-                }).thenApply(integer -> integer + 1000)
-                .thenAccept(System.out::println)
-                .thenRun(() -> {
-                    System.out.println("Tugadiiii.....");
-                });
-        Thread.sleep(500);
+
     }
 }
